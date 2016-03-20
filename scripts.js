72e9db9e329f37538c545f74e94368a1908f28e9
SaryDelaA <sarycdo@gmail.com>
2016-03-20 15:39:51 -0500

    corrigiendo tus errores

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 console.log("Inicio de programa");
 alert("hola JavaScript");
-document.write("<h1>Es un saludo inicial</h1>";
-alert("Gracias por visitarnos";
+document.write("<h1>Es un saludo inicial</h1>");
+alert("Gracias por visitarnos");
 console.log("fin de programa");
